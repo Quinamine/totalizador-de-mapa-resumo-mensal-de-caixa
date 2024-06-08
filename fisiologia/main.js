b1dd9ef9783bba348ddb09e0e53af187bda20e8b
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-06-08 10:08:28 -0300

    JS: Desenvolvi o algorítimo que calcula dos totais no eixo y da coluna 'Valor Total' concluindo dessa forma a funcionalidade de totalização.

--- fisiologia/main.js
+++ fisiologia/main.js
@@ -14,36 +14,20 @@ function backup() {
 
 const totalizador = {
     filtrarEtotalizarCelulas(inputTarget) {
-        if(inputTarget.dataset.cobradoousubsidiado) {
-            const algarismosDeCobradoOuSubsidiado = document.querySelectorAll(`.${inputTarget.dataset.cobradoousubsidiado}`);
-            const contraAlgarismos = document.querySelectorAll(`.${inputTarget.dataset.cobradoousubscorrespondente}`);
+        if(inputTarget.dataset.contraquarteto) {
+            const algarismosDoQuartetoCelular = document.querySelectorAll(`.${inputTarget.dataset.quartetocelular}`);
+            const contraAlgarismos = document.querySelectorAll(`.${inputTarget.dataset.contraquarteto}`);
 
-            let valCobradoOuSubsidiado = "", contraValor = "";
-            for (let i=0; i < algarismosDeCobradoOuSubsidiado.length; i++) {
-                valCobradoOuSubsidiado += algarismosDeCobradoOuSubsidiado[i].value;
+            let valorDoQuartetoCelular = "", contraValor = "";
+            for (let i=0; i < algarismosDoQuartetoCelular.length; i++) {
+                valorDoQuartetoCelular += algarismosDoQuartetoCelular[i].value;
                 contraValor += contraAlgarismos[i].value;
 
             }
-            let total = Number(valCobradoOuSubsidiado) + Number(contraValor);
+            let total = Number(valorDoQuartetoCelular) + Number(contraValor);
             total = total.toString();
             const celulasDeSaida = document.querySelectorAll(`.${inputTarget.dataset.totaleixoxoutput}`);
-            
-            /*celulasDeSaida[3].value = total[total.length - 1]
-            celulasDeSaida[2].value = total[total.length - 2]
-            celulasDeSaida[1].value = total[total.length - 3]
-            celulasDeSaida[0].value = total[total.length - 4]*/
-
-            for (const celula of celulasDeSaida) celula.value = "";
-
-            let outputCellIndex = 3;
-            for (let i=1; i <= total.length; i++) {
-                if(outputCellIndex >= 0) celulasDeSaida[outputCellIndex].value = total[total.length - i];
-                outputCellIndex--;
-                if(total.length > 4) {
-                    let milhares = total.substr(0, total.length - 3);
-                    celulasDeSaida[0].value = Number(milhares).toLocaleString();
-                }
-            }
+            this.preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida);
 
         } 
 
@@ -57,14 +41,13 @@ const totalizador = {
             celulaDeSaida.value = this.somar(operandos);
         } else {
             if(inputTarget.dataset.totaleixoy) {
-                const algarismos = document.querySelectorAll(`.${inputTarget.dataset.cobradoousubsidiado}`);
                 const operandos = document.querySelectorAll(`.${inputTarget.dataset.totaleixoy}`)
 
 
                 let contraAlgarismos, contraValor = "";
                 let total = 0;
                 for (let i=0; i < operandos.length; i+=4) {
-                    contraAlgarismos = document.querySelectorAll(`.${operandos[i].dataset.cobradoousubsidiado}`);
+                    contraAlgarismos = document.querySelectorAll(`.${operandos[i].dataset.quartetocelular}`);
                     for (const algarismo of contraAlgarismos) {
                         contraValor += algarismo.value;
                     }
@@ -74,28 +57,42 @@ const totalizador = {
 
                 total = total.toString();
                 const celulasDeSaida = document.querySelectorAll(`.${inputTarget.dataset.totaleixoyoutput}`);
-                
-                /*celulasDeSaida[3].value = total[total.length - 1]
-                celulasDeSaida[2].value = total[total.length - 2]
-                celulasDeSaida[1].value = total[total.length - 3]
-                celulasDeSaida[0].value = total[total.length - 4]*/
-
-                for (const celula of celulasDeSaida) celula.value = "";
-
-                let outputCellIndex = 3;
-                for (let i=1; i <= total.length; i++) {
-                    if(outputCellIndex >= 0) celulasDeSaida[outputCellIndex].value = total[total.length - i];
-                    outputCellIndex--;
-                    if(total.length > 4) {
-                        let milhares = total.substr(0, total.length - 3);
-                        celulasDeSaida[0].value = Number(milhares).toLocaleString();
-                    }
-                }
-
-          
-                    
-            }       
+                this.preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida);
+            }
         }
+
+        if(inputTarget.dataset.totalgeral) {
+            const operandos = document.querySelectorAll(`.${inputTarget.dataset.totalgeral}`)
+
+            let contraAlgarismos, contraValor = "";
+            let total = 0;
+            for (let i=0; i < operandos.length; i+=4) {
+                contraAlgarismos = document.querySelectorAll(`.${operandos[i].dataset.quartetocelular}`);
+                for (const algarismo of contraAlgarismos) {
+                    contraValor += algarismo.value;
+                }
+                total+= Number(contraValor)
+                contraValor = ""
+            }
+
+            total = total.toString();
+            const celulasDeSaida = document.querySelectorAll(`.${inputTarget.dataset.totalgeraloutput}`);
+            this.preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida);
+        }
+    },
+
+    preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida) {
+        for (const celula of celulasDeSaida) celula.value = "";
+
+            let outputCellIndex = 3;
+            for (let i=1; i <= total.length; i++) {
+                if(outputCellIndex >= 0) celulasDeSaida[outputCellIndex].value = total[total.length - i];
+                outputCellIndex--;
+                if(total.length > 4) {
+                    let milhares = total.substr(0, total.length - 3);
+                    celulasDeSaida[0].value = Number(milhares).toLocaleString();
+                }
+            }
     },
     
     somar(celulasPorTotalizar) {
@@ -112,8 +109,9 @@ function escutarEventos() {
     inputsCelulares.forEach( inputCelular => {
         inputCelular.addEventListener("input", () => totalizador.filtrarEtotalizarCelulas(inputCelular));
         inputCelular.classList.add(`${inputCelular.dataset.totaleixoy}`);
-        if(inputCelular.dataset.cobradoousubsidiado) {
-            inputCelular.classList.add(`${inputCelular.dataset.cobradoousubsidiado}`);
+        inputCelular.classList.add(`${inputCelular.dataset.totalgeral}`);
+        if(inputCelular.dataset.quartetocelular) {
+            inputCelular.classList.add(`${inputCelular.dataset.quartetocelular}`);
             //inputCelular.value !== "" && totalizador.filtrarEtotalizarCelulas(inputCelular);
         }
     });
