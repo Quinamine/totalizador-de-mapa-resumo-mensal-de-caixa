9e2c97dcaec65c6ef40309d69553ba1da40a09f7
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2024-08-21 20:15:22 -0300

    HTML/CSS/JS: Minifiquei todos os arquivos tirando os espaços entre os blocos e entre as linhas dentro de alguns blocos.

--- fisiologia/main.js
+++ fisiologia/main.js
@@ -2,49 +2,38 @@
 
 function backup() {
     const inputsGerais = document.querySelectorAll(".ficha input, textarea");
-
     for (let i = 0; i < inputsGerais.length; i++) {
-        
         inputsGerais[i].addEventListener("input", () => {
             localStorage.setItem(`${keyPrefix}-input${i}`, inputsGerais[i].value);
         });  
         inputsGerais[i].value = localStorage.getItem(`${keyPrefix}-input${i}`);
     }
 }
-
 const totalizador = {
     filtrarEtotalizarCelulas(inputTarget) {
         if(inputTarget.dataset.totaleixoxoutput) {
             const algarismosDoQuartetoCelular = document.querySelectorAll(`.${inputTarget.dataset.quartetocelular}`);
             const contraAlgarismos = document.querySelectorAll(`.${inputTarget.dataset.contraquarteto}`);
-
             let valorDoQuartetoCelular = "", contraValor = "";
             for (let i=0; i < algarismosDoQuartetoCelular.length; i++) {
                 valorDoQuartetoCelular += algarismosDoQuartetoCelular[i].value;
                 contraValor += contraAlgarismos[i].value;
-
             }
             let total = Number(valorDoQuartetoCelular) + Number(contraValor);
             total = total.toString();
             const celulasDeSaida = document.querySelectorAll(`.${inputTarget.dataset.totaleixoxoutput}`);
             this.preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida);
-
         } 
-
         if(inputTarget.parentElement.dataset.coltitle === "receitas-aviadas") {
             let classNameDosOperandos = inputTarget.dataset.totaleixoy;
             inputTarget.classList.add(`${classNameDosOperandos}`);
-
             const operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
             const celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totaleixoyoutput}`);
             celulaDeSaida.value = this.somar(operandos);
             return false;
         }
-        
         if(inputTarget.dataset.totaleixoy) {
             const operandos = document.querySelectorAll(`.${inputTarget.dataset.totaleixoy}`)
-
-
             let contraAlgarismos, contraValor = "";
             let total = 0;
             for (let i=0; i < operandos.length; i+=4) {
@@ -55,15 +44,12 @@ const totalizador = {
                 total+= Number(contraValor)
                 contraValor = ""
             }
-
             total = total.toString();
             const celulasDeSaida = document.querySelectorAll(`.${inputTarget.dataset.totaleixoyoutput}`);
             this.preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida);
         }
-
         if(inputTarget.dataset.totalgeral) {
             const operandos = document.querySelectorAll(`.${inputTarget.dataset.totalgeral}`)
-
             let contraAlgarismos, contraValor = "";
             let total = 0;
             for (let i=0; i < operandos.length; i+=4) {
@@ -74,27 +60,23 @@ const totalizador = {
                 total+= Number(contraValor)
                 contraValor = ""
             }
-
             total = total.toString();
             const celulasDeSaida = document.querySelectorAll(`.${inputTarget.dataset.totalgeraloutput}`);
             this.preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida);
         }
     },
-
     preecherTotalAoQuartetoCelularDeSaida(total, celulasDeSaida) {
         for (const celula of celulasDeSaida) celula.value = "";
-
-            let outputCellIndex = 3;
-            for (let i=1; i <= total.length; i++) {
-                if(outputCellIndex >= 0) celulasDeSaida[outputCellIndex].value = total[total.length - i];
-                outputCellIndex--;
-                if(total.length > 4) {
-                    let milhares = total.substr(0, total.length - 3);
-                    celulasDeSaida[0].value = Number(milhares).toLocaleString();
-                }
+        let outputCellIndex = 3;
+        for (let i=1; i <= total.length; i++) {
+            if(outputCellIndex >= 0) celulasDeSaida[outputCellIndex].value = total[total.length - i];
+            outputCellIndex--;
+            if(total.length > 4) {
+                let milhares = total.substr(0, total.length - 3);
+                celulasDeSaida[0].value = Number(milhares).toLocaleString();
             }
+        }
     },
-    
     somar(celulasPorTotalizar) {
         let soma = 0;
         for(const c of celulasPorTotalizar) {
@@ -114,11 +96,8 @@ function escutarEventos() {
         setTimeout(() => {
             inputCelular.value !== "" && totalizador.filtrarEtotalizarCelulas(inputCelular);
         }, 0)
- 
-        
     });
 }
-
 window.addEventListener("load", () => {
     backup();
     escutarEventos();    
--- fisiologia/referencia.js
+++ fisiologia/referencia.js
@@ -1,37 +1,28 @@
 "use strict"
-
 const referencia = {
     retornarLinha(inputTarget) {
         const colLinhas = document.querySelectorAll(".ficha__body__col-de-dias span");
         const linhaOuput = document.querySelector(".reference__output--linha");
         const inputTargetAndSiblings = inputTarget.parentElement.children;
         let inputTargetIndex = 0;
-
         for (let i=0; i < inputTargetAndSiblings.length; i++) {
             if(inputTargetAndSiblings[i] === inputTarget) {
                 inputTargetIndex = i;
             }
         }
-
         linhaOuput.value = colLinhas[inputTargetIndex].innerText;
-
     },
-
     retornarColuna(inputTarget) {
         const indicadorOutput = document.querySelector(".reference__output--coluna");
-
         let classeDoIndicadorColunar = inputTarget.parentElement.dataset.coltitle;
         let indicadorColunar = document.querySelector(`.${classeDoIndicadorColunar}`).innerText;
         indicadorOutput.innerHTML = indicadorColunar; 
-
     },
-
     retornarVazio() {
         const outputs = document.querySelectorAll(".reference__output");
         for (const o of outputs) o.value = "";
     }
 }
-
 function events() {
     const inputsCelulares = document.querySelectorAll(".ficha__col-de-inputs input");
     inputsCelulares.forEach( inputCelular => {
@@ -42,8 +33,6 @@ function events() {
             }
         });
     });
-
     inputsCelulares.forEach( inputCelular => inputCelular.addEventListener("focusout", referencia.retornarVazio));
 }
-
 window.onload = events;
